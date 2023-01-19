2e75782e81e6d66b9116980827ea92811f97588f
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2023-01-19 17:26:40 +0100

    feat: add serializers

---
+++ app/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from .models import User, Post, Achievement, Advertising
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """Post serializer."""
+
+    class Meta:
+        model = Post
+        fields = ("title", "date_create", "content")
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    """Achievement serializer."""
+
+    class Meta:
+        model = Achievement
+        fields = ("title", "condition", "image")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Nested user serializer with all events."""
+
+    post = PostSerializer(many=True)
+    achievement = AchievementSerializer(many=True)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "post", "achievement")
+
+
+class AdvertisingSerializer(serializers.ModelSerializer):
+    """Advertising serializer."""
+
+    class Meta:
+        model = Advertising
+        fields = "__all__"  # ('title', 'content', 'image', 'url', 'date_create')
